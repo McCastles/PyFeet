5c6b8b119004d5af316f6b2ffca38f5ea8454e70
bazyli <b.reps@optimali.io>
2020-01-16 21:35:31 +0100

    initial commit for develop

---
+++ walk_monitoring/source/main_app.py
@@ -0,0 +1,28 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import pandas as pd
+import json
+import requests
+
+
+def main():
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div(children=[
+        html.H4(id='header', children='Nasz Header'),
+        html.Div(id='content', children='')
+    ])
+
+    @app.callback(
+        [Output('content', 'children')]
+    )
+    def say_hello():
+        return [html.Div(children='Hello!')]
+
+    app.run_server(debug=True)
+
+
+if __name__ == '__main__':
+    main()
