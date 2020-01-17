aee8432f91489f8b836a758f067e572b52b986e9
bazyli <b.reps@optimali.io>
2020-01-18 00:48:45 +0100

    added basic session storage, sensor trace building and basic trace plotting

--- walk_monitoring/source/main_app.py
+++ walk_monitoring/source/main_app.py
@@ -2,12 +2,53 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import pandas as pd
 import json
 import requests
+import numpy as np
+import time
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 person_ids = [1, 2, 3, 4, 5, 6]
+x_range = 100
+n_traces = 6
+left_trace_range = range(3)
+right_trace_range = range(3, 6)
+
+
+def prepare_array(series, value):
+    if len(series) < x_range:
+        arr = np.zeros(x_range)
+        np.put(arr, np.arange(len(series)), series)
+        series = arr# .tolist()
+    else:
+        series = series[1:]
+        series.append(value)
+    return series
+
+
+def create_figure(traces, current_range, title):
+    xticks = np.arange(x_range)
+    return {
+        'data': [{
+            'x': xticks,
+            'y': series,
+            'name': f'sensor{list(current_range)[i] + 1}',
+            'text': f'sensor{list(current_range)[i] + 1}'
+        } for i, series in enumerate(traces)],
+        'layout': {
+            'title': title,
+            'yaxis': {
+                'title': 'sensor value'
+            }
+        }
+    }
+
+
+def build_data_storage_dict():
+    store = {'user_id': 1}
+    for i in range(n_traces):
+        store[f'trace{i}'] = []
+    return store
 
 
 def main():
@@ -15,48 +56,87 @@ def main():
 
     app.layout = html.Div(children=[
         html.H4(id='header', children='Nasz Header'),
-        html.Button('Id 1', id='button1'),
-        html.Button('Id 2', id='button2'),
-        html.Button('Id 3', id='button3'),
-        html.Button('Id 4', id='button4'),
-        html.Button('Id 5', id='button5'),
-        html.Button('Id 6', id='button6'),
+        html.Div(id='changed', children=''),
+        *[html.Button(f'Id {i}', id=f'button{i}') for i in range(1, 7)],
         html.Div(id='content', children=''),
         dcc.Interval(
             id='clock',
-            interval=1000,
+            interval=500,
             n_intervals=0
+        ),
+        dcc.Store('current_user_id_input', data=build_data_storage_dict()),
+        dcc.Store('current_user_id_output', data=build_data_storage_dict()),
+        dcc.Graph(
+            id='trace_left',
+            figure={
+                'data': [{
+                    'x': np.arange(x_range),
+                    'name': 'Trace'
+                }]
+            }
+        ),
+        dcc.Graph(
+            id='trace_right',
+            figure={
+                'data': [{
+                    'x': np.arange(x_range),
+                    'name': 'Trace'
+                }]
+            }
         )
     ])
 
     @app.callback(
-        [Output('content', 'children')],
+        [Output('content', 'children'),
+         Output('current_user_id_output', 'data'),
+         Output('changed', 'children'),
+         Output('trace_left', 'figure'),
+         Output('trace_right', 'figure')],
         [Input('clock', 'n_intervals'),
-         Input('button1', 'n_clicks_timestamp'),
-         Input('button2', 'n_clicks_timestamp'),
-         Input('button3', 'n_clicks_timestamp'),
-         Input('button4', 'n_clicks_timestamp'),
-         Input('button5', 'n_clicks_timestamp'),
-         Input('button6', 'n_clicks_timestamp'),
-         ]
+         *[Input(f'button{i}', 'n_clicks_timestamp') for i in range(1, 7)],
+         ],
+        [State('current_user_id_input', 'data')]
 
     )
-    def tick(n_intervals, t1, t2, t3, t4, t5, t6):
+    def tick(_, t1, t2, t3, t4, t5, t6, data):
         buttons_times = [t1, t2, t3, t4, t5, t6]
         for i, time in enumerate(buttons_times):
             buttons_times[i] = 0 if time is None else time
         max_val = max(buttons_times)
-        current_id = buttons_times.index(max_val) + 1
-        url = f'http://tesla.iem.pw.edu.pl:9080/v2/monitor/{current_id}'
+        user_id = buttons_times.index(max_val) + 1
+
+        changed = user_id != data['user_id']
+
+        if changed:
+            data['user_id'] = user_id
+            for i in range(n_traces):
+                data[f'trace{i}'] = []
+
+        url = f'http://tesla.iem.pw.edu.pl:9080/v2/monitor/{user_id}'
         json_data = json.loads(requests.get(url).text)
-        print(n_intervals)
         sensors = json_data['trace']['sensors']
         sensor_paragraphs = [html.P(f'id: {s["id"]}, value: {s["value"]} anomaly: {s["anomaly"]}, ') for s in sensors]
-        return [html.Div([
+
+        for i in range(n_traces):
+            series = prepare_array(data[f'trace{i}'], sensors[i]['value'])
+            data[f'trace{i}'] = series
+        fig_left = create_figure([data[f'trace{i}'] for i in left_trace_range], left_trace_range,
+                                 'Left Foot Sensors Trace')
+        fig_right = create_figure([data[f'trace{i}'] for i in right_trace_range], right_trace_range,
+                                  'Right Foot Sensors Trace')
+
+        return html.Div([
             html.H1('Name:'),
             html.P(f"{json_data['firstname']} {json_data['lastname']}"),
             *sensor_paragraphs
-        ])]
+        ]), data, html.H1(str(changed)), fig_left, fig_right
+
+    @app.callback(
+        Output('current_user_id_input', 'data'),
+        [Input('current_user_id_output', 'data')]
+    )
+    def store_current_user_id(data):
+        return data
 
     app.run_server(debug=True)
 
